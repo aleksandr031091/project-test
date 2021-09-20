f3ccf405078100558c4d90cca10571ac6235c208
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-09-20 19:29:28 +0300

    str tw initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Stopwotch from "./stopwatch/Stopwatch";
+
+const App = () => {
+  return (
+    <>
+      <Stopwotch />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/stopwatch/Stopwatch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Stopwatch = () => {
+  return (
+    <>
+      <h2>good sw</h2>
+    </>
+  );
+};
+
+export default Stopwatch;
